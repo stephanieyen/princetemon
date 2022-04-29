62f69996e985bc49eb86dd127a237235acadcfb5
stephanieyen <steph.yensy@gmail.com>
2022-04-29 17:39:59 -0400

    Prospect tile set in progress with code refactoring to simplify adding houses/logos, waiting for Piskels before adding images

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -6,7 +6,6 @@ export { default as Serene } from './serene.png';
 export { default as Sprites } from './sprites.png';
 export { default as PoeGameTiles } from './poe_game.png';
 export { default as Rocks } from './rocks.png';
-export { default as Street } from './prospect.png';
 export { default as SpritePton } from './player_sprite.png';
 export { default as FristSmall } from './Frist.png';
 export { default as PoeField } from './poe.png';
@@ -22,5 +21,28 @@ export { default as CannonGreen } from './cannon_green.png';
 export { default as Flowers } from './flowers.png';
 export { default as Fountain } from './fountain.png'
 export { default as Nassau } from './nassau_hall.png';
-export { default as Ivy } from './prospect_ivy.png';
-export { default as IvyLogo } from './ivy.png';
+
+export { default as Street } from './prospect/prospect.png';
+export { default as Vehicles } from './vehicles.png';
+
+// Eating clubs
+// export { default as Tower } from './prospect/tower.png';
+export { default as TowerLogo } from './prospect/tower_logo.png';
+// export { default as Cannon } from './prospect/cannon.png';
+// export { default as CannonLogo } from './prospect/cannon_logo.png';
+// export { default as Quad } from './prospect/quad.png';
+// export { default as QuadLogo } from './prospect/quad_logo.png';
+// export { default as Colonial } from './prospect/colonial.png';
+// export { default as ColonialLogo } from './prospect/colonial_logo.png';
+export { default as Ivy } from './prospect/ivy.png';
+export { default as IvyLogo } from './prospect/ivy_logo.png';
+export { default as TigerInn } from './prospect/ti.png';
+export { default as TigerInnLogo } from './prospect/ti_logo.png';
+// export { default as Cottage } from './prospect/cottage.png';
+// export { default as CottageLogo } from './prospect/cottage_logo.png';
+// export { default as Cap } from './prospect/cap.png';
+export { default as CapLogo } from './prospect/cap_logo.png';
+// export { default as Cloister } from './prospect/Cloister.png';
+// export { default as CloisterLogo } from './prospect/cloister_logo.png';
+// export { default as Charter } from './prospect/Charter.png';
+// export { default as CharterLogo } from './prospect/charter_logo.png';
